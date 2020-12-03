5fbd03a169d60b147bd5c4ab34a6011013dc509c
Alex Palomes <alejandropalomes@hotmail.com>
2020-12-03 23:59:34 +0100

    tests implementation

--- src/components/tests/Login.test.tsx
+++ src/components/tests/Login.test.tsx
@@ -1,33 +1,106 @@
-import '@testing-library/jest-dom';
-import 'react-testing-library/cleanup-after-each'
+
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import '@testing-library/jest-dom';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+
 import Login from '../Login';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Login Component test suite', () => {
 
-describe('App', () => {
-    test('renders App component', async () => {
-        render(<Login setToken = {()=>{}} />);
 
-        // wait for the user to resolve
-        // needs only be used in our special case
-        await screen.findByText(/Login/);
+    const getter = 'property';
+    const setter = jest.fn();
+    const defaultProps = [getter, setter];
+    const component = (
+        <Router>
+            <Login setToken={defaultProps}/>
+        </Router>
+    )
+    const setup = () => render(component);
 
-        expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    it('Renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(component, div);
+        ReactDOM.unmountComponentAtNode(div)
+    })
 
-        fireEvent.change(screen.getByRole('textbox'), {
-            target: { value: 'JavaScript' },
-        });
+    it('Should match snapshot', () => {
+        const { container } = setup();
+        expect(container).toMatchSnapshot();
+    })
 
-        expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
-    });
-  });
+    it('Should return error message on email when it is not an email', async ()=>{
+        const { getByText, getByPlaceholderText } = setup();
+        const loginButton = getByText('Login');
+        const emailInput = getByPlaceholderText('Email');
+        const passwordInput = getByPlaceholderText('Password');
+        fireEvent.change(emailInput, { target: { value: '23' } })
+        fireEvent.change(passwordInput, { target: { value: '23' } })
+        fireEvent.click(loginButton)
 
-test('checks if enable or disable login button', () => {
+        expect(getByText('Must be an email: email@domain')).toBeDefined();
+    })
 
+    it('Should return error message on password when it does not match all conditions', async ()=>{
+        const { getByText, getByPlaceholderText } = setup();
+        const loginButton = getByText('Login');
+        const emailInput = getByPlaceholderText('Email');
+        const passwordInput = getByPlaceholderText('Password');
+        fireEvent.change(emailInput, { target: { value: 'admin@email.com' } })
+        fireEvent.change(passwordInput, { target: { value: '23' } })
+        fireEvent.click(loginButton)
+
+        expect(getByText('Conditions: 8 char long, 1 lowercase, 1 uppercase, 1 number & 1 special character.')).toBeDefined();
+    })
+
+    it('Should return error message on email when user not found', async ()=>{
+
+        const { getByText, getByPlaceholderText } = setup();
+        const loginButton = getByText('Login');
+        const emailInput = getByPlaceholderText('Email');
+        const passwordInput = getByPlaceholderText('Password');
+
+        fireEvent.change(emailInput, { target: { value: 'user@email.com' } })
+        fireEvent.change(passwordInput, { target: { value: 'Pa$$w0rd' } })
+        fireEvent.click(loginButton)
+
+        await waitFor(()=>{
+            expect(getByText('User not found.')).toBeDefined();
+        }, {timeout: 2000})
+    })
+
+    it('Should return error message on password when user found but password is not correct', async ()=>{
+
+        const { getByText, getByPlaceholderText } = setup();
+        const loginButton = getByText('Login');
+        const emailInput = getByPlaceholderText('Email');
+        const passwordInput = getByPlaceholderText('Password');
+
+        fireEvent.change(emailInput, { target: { value: 'admin@email.com' } })
+        fireEvent.change(passwordInput, { target: { value: 'Pa$$w0rdddd' } })
+        fireEvent.click(loginButton)
+
+        await waitFor(()=>{
+            expect(getByText('Sorry, wrong password.')).toBeDefined();
+        }, {timeout: 2000})
+    })
+
+    it('Should give access if correct email and password', async ()=>{
+
+        const { getByText, getByPlaceholderText, queryByText } = setup();
+        const loginButton = getByText('Login');
+        const emailInput = getByPlaceholderText('Email');
+        const passwordInput = getByPlaceholderText('Password');
+
+        fireEvent.change(emailInput, { target: { value: 'admin@email.com' } })
+        fireEvent.change(passwordInput, { target: { value: 'Pa$$w0rd' } })
+        fireEvent.click(loginButton)
+
+        await waitFor(()=>{
+            expect(queryByText('Sorry, wrong password.')).toBeNull();
+            expect(queryByText('User not found.')).toBeNull();
+        }, {timeout: 2000})
+    })
 });
